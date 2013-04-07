8cace853ce1ba058072a9ad21b630aace1385a21
Christopher Valles <info@christophervalles.com>
2013-04-07 11:24:49 +0100

    Initial code - Chapter 9

---
+++ Chapter 9/api/module/Wall/config/module.config.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+return array(
+    'router' => array(
+        'routes' => array(
+            'wall' => array(
+                'type' => 'Zend\Mvc\Router\Http\Segment',
+                'options' => array(
+                    'route'    => '/api/wall[/:id]',
+                    'constraints' => array(
+                        'id' => '\w+'
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Wall\Controller\Index'
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'di' => array(
+        'services' => array(
+            'Wall\Model\UsersTable' => 'Wall\Model\UsersTable',
+            'Wall\Model\UserStatusesTable' => 'Wall\Model\UserStatusesTable',
+            'Wall\Model\UserImagesTable' => 'Wall\Model\UserImagesTable',
+            'Wall\Model\UserLinksTable' => 'Wall\Model\UserLinksTable'
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Wall\Controller\Index' => 'Wall\Controller\IndexController'
+        ),
+    ),
+    'akismet' => array(
+        'apiKey' => '010b675653dd',
+        'url' => 'http://zf2-test-book.com'
+    )
+);
