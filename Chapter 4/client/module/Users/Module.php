0b920635654543408d18f2fbfe4949a36cede0e9
Christopher Valles <info@christophervalles.com>
2013-09-12 21:58:16 +0100

    CHAPTER 4 - Removing unused stuff

--- Chapter 4/client/module/Users/Module.php
+++ Chapter 4/client/module/Users/Module.php
@@ -9,19 +9,8 @@
 
 namespace Users;
 
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-
 class Module
 {
-    public function onBootstrap(MvcEvent $e)
-    {
-        $e->getApplication()->getServiceManager()->get('translator');
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
