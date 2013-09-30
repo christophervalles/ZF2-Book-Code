9d5f9373bd501e62ffd4de84ac4e3ec8a2908e29
Christopher Valles <info@christophervalles.com>
2013-09-30 23:48:01 +0100

    CHAPTER 11, 12 and 13 - Propagate fix on avatar upload

--- Chapter 13/api/module/Users/src/Users/Controller/IndexController.php
+++ Chapter 13/api/module/Users/src/Users/Controller/IndexController.php
@@ -81,7 +81,7 @@ class IndexController extends AbstractRestfulController
         if ($filters->isValid()) {
             $data = $filters->getValues();
             
-            $avatarContent = $unfilteredData['avatar'];
+            $avatarContent = array_key_exists('avatar', $unfilteredData) ? $unfilteredData['avatar'] : NULL;
             
             $bcrypt = new Bcrypt();
             $data['password'] = $bcrypt->create($data['password']);
